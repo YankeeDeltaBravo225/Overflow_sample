69bc9fd29c5c64534bdffde56f6836800fe063f7
YankeeDeltaBravo225 <31369295+YankeeDeltaBravo225@users.noreply.github.com>
2018-05-03 23:54:36 +0900

    Added integer overflow samples

---
+++ src/integer_overflow_gt_max.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void main(void)
+{
+    uint8_t a = 255;
+    printf("a:%d\n", a);
+
+    a++;
+    printf("a:%d\n", a);
+}
---
+++ src/integer_overflow_lt_min.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void main(void)
+{
+    uint8_t b = 0;
+    printf("b:%d\n", b);
+
+    b--;
+    printf("b:%d\n", b);
+}
